328e006bb278c4c64d452b98f643daa62f144ea4
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-01-01 10:48:47 -0500

    Update index.js
     - Added submit button onclick event listner via arrow function

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 
+// Get the submit button off the screen and attach a onclick listenr for it
+let submit = document.getElementById("submit");
+
+// Arrow funciton attached to the submit button for onclick action events
+submit.onclick = () => {
+  console.log("Reponse submitted!");
+}
